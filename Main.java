1631a0a9098675e98df69ec52274506118691949
Raymour <raymour@hotmail.com>
2016-06-27 09:02:31 -0400

    codingbat

---
+++ Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    //http://codingbat.com/prob/p187868
+
+        public boolean sleepIn ( boolean weekday, boolean vacation){
+            if (!weekday || vacation) ;
+            return true;
+
+            else false;
+
+        }
+    //http://codingbat.com/prob/p154485
+    {
+        public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if (a == b){
+            sum = sum * 2;
+        }
+        System.out.println(sum);
+    }
+
+    }
+
+    //http://codingbat.com/prob/p172021
+
+    public int close10(int a, int b) {
+        int aTest = Math.abs(a-10);
+        int bTest = Math.abs(b-10);
+
+        if (aTest > bTest); {
+            return b;
+        }
+        else (bTest > aTest);{
+            return a;
+        }
+        else if {
+            bTest == aTest
+            return 0;
+        }
+
+
+    }
+
+
+
+
+
